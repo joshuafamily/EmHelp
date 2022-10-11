b82c60d80508a7b76f45814bceb9e16713cd140e
xiaomagua <1170556710@qq.com>
2022-10-11 21:29:28 +0800

    test release

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by chongchong ding on 10/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //self.view.backgroundColor = UIColor.purple
+    }
+
+
+}
+
